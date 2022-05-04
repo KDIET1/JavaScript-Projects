5d64b014097fb232f4ebb0e45e7d981897a49d4d
KDIET1 <102564890+KDIET1@users.noreply.github.com>
2022-05-03 18:02:30 -0700

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+var A = "This is a string"; //This is a string value
+document.write(A)
+var X = "Hello, World!"; //This is defining a variable
+document.write(X)
+window.alert(X) //This is an alert window
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded."); //This is learning how to add quotes and apostrophes
+document.write("\" Be who you are and say what you feel,"+"because those who mind don\'t matter and those who matter don\'t mind.\""+"-Dr. Seuss");//This is Concatenating a string
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermione", Daughter = "Penny", Son = "Zorro"; //This is defining multiple variables
+document.write(Dad); //This is returning a specified variable
+document.write(Mom);
+document.write(3+3);//This is an expression
