67611db9dae86373b7931dd8dae908098495fbb8
KDIET1 <102564890+KDIET1@users.noreply.github.com>
2022-05-03 18:26:19 -0700

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function My_First_Function() { //This is my first function
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+function myFunction() { //This is my +=operator Function
+    var sentence="I am learning";
+    sentence+=" a lot from this book!";
+    document.getElementById("Concatenate").innerHTML=sentence;
+}
