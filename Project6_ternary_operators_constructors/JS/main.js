eb4eefcb6f5641be5bc439b24a5366be1981dbc3
KDIET1 <102564890+KDIET1@users.noreply.github.com>
2022-05-09 16:48:57 -0700

    Updated Project 6

--- Project6_ternary_operators_constructors/JS/main.js
+++ Project6_ternary_operators_constructors/JS/main.js
@@ -1,8 +1,8 @@
-function Ride_function() { //Ternary Operator
+function Ride_Function() {
     var Height, Can_ride;
-    Height=document.getElementById("Height").value;
-    Can_ride=(Height<52)?"You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML=Can_ride+"to ride.";
+    Height= document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough.";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 function Vehicle (Make, Model, Year, Color) { //Keywords and Constructors
     this.Vehicle_Make=Make;
