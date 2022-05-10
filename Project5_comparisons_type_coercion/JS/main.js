c19fc1618e5dadad758f72b0d9d020e3d107fc30
KDIET1 <102564890+KDIET1@users.noreply.github.com>
2022-05-09 17:37:11 -0700

    Updated Project 5

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -1,31 +1,34 @@
-document.write(typeof "Word"); //Typeof Operator string
-document.write(typeof 3); //Typeof operator number
-document.write(typeof "Word" 3) //Typeof operator word and string
-document.write("10"+5); //Type coercion
-function my_Function() {
-    document.getElementById("Test").innerHTML=0/0; //NaN false
-    document.getElementById("Test").innerHTML=isNaN('This is a string'); //NaN True
-    document.getElementById("Test").innerHTML=isNaN('007'); //NaN false
+document.write(typeof "Word"); //String
+document.write(typeof 3); //number
+document.write("10"+5); //coercion
+
+function nan_Function() {
+    document.getElementById("Test").innerHTML=isNaN('808'); //NaN function
 }
 document.write(2E310); //Infinity
 document.write(-3E310); //negative Infinity
-document.write(10>2); //Boolean true
-document.write(10<2); //Boolean false
-console.log(2+2); //Console Log
-document.write(10==10); //number on left equals number on right
-document.write(3==11); //number on left doesn't equal number on right
-X=10;
-Y=10;
-document.write(X===Y); //both type and value are true
-X=82;
-Y="82";
-document.write(X===Y); //value is true but type is false
-document.write(5>2&&10>4); //Boolean logic operator AND
-document.write(5>10&&10>4); //Boolean logic operator AND
-document.write(5>10||10>4); //Boolean logic operator OR
-function not_Function() {
-    document.getElementById("Not").innerHTML=!(20>10); //NOT operator true
+document.write(8 > 3); //Boolean True
+document.write(8 < 3); //Boolean False
+console.log(2+2); //Console log
+document.write(12 == 12); //double equal sign true
+document.write(12 == 11); //double equal sign false
+
+X = 45; //triple equal sign false
+Y = "FOX";
+document.write(X === Y);
+A = 85;
+B = 85;
+document.write(A === B); //triple equal sign true
+
+document.write(7 > 3 && 9 > 5); //AND operator true
+document.write(7 > 3 && 5 > 9); //AND operator false
+
+document.write(10 > 8 || 7 > 4); //OR operator true
+document.write(10 < 8 || 7 < 4); //OR operator false
+
+function not_Function() { //NOT function false
+    document.getElementById("Not").innerHTML = !(18 > 14);
 }
-function not_Function() {
-    document.getElementById("Not").innerHTML=!(5>10); //NOT operator false
+function not_Function2() { //NOT function true
+    document.getElementById("Not2").innerHTML = !(20 < 13);
 }
